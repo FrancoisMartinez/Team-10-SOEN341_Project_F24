c80417b19a1c0de8a020306d91afb1863573f73c
Ayman <mehd.ayman@gmail.com>
2024-09-27 19:14:50 -0400

    Update studentLogin.php

--- BackEnd/studentLogin.php
+++ BackEnd/studentLogin.php
@@ -1,3 +1,18 @@
-<? php
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset( $_POST["submit"] ) ){
+        $conn = mysqli_connect("localhost", "root", "","peerreview") or die("Connection Failed:" . mysqli_connect_error());
+        if(isset($_POST["email"]) && isset($_POST["password"])){
+            $email=$_POST["email"];
+            $password=$_POST["password"];
 
-$connect
+            $sql= "INSERT INTO `students` (`email`, `password`) VALUES ('$email', '$password)";
+        
+            $query = mysqli_query($conn,$sql);
+            if($query){
+                echo "Entry Successfull";
+            }
+            else{
+                echo "Error Occured in query";
+            }
+        }
+    }
