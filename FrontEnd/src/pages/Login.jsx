b17e7338bbb1637dd044f182c88a34e5baa1ace4
HaichuanLi <lihaichuan2011@hotmail.com>
2024-09-28 16:53:29 -0400

    Student login page

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -11,9 +11,36 @@ function Login() {
 
     return (
         <>
-            <div>
-                <Header/>
-                
+            <div className={styles.background}>
+                <div className={styles.loginWindow}>
+                    <div className={styles.loginWindowLeft}>
+
+
+                        <img className={styles.logo} src="/Logo.webp" alt="Logo" width="150" height="150" />
+                        <h1 className={styles.siteName}>Rate My Teammates</h1>
+
+
+                    </div>
+                    <div className={styles.loginWindowRight}>
+                        <h1 className={styles.studentLogIn}>Student Login</h1>
+                        <div className={styles.emailBox}>
+                            <img className={styles.emailLogo} src="/email_logo.png" alt="email logo" width="30" height="30" />
+                            <h2 className={styles.emailTag}>Email</h2>
+                        </div>
+                        <div className= {styles.passBox}>
+                            <img className={styles.passLogo} src="/password_logo.png" alt="password logo" width="30" height="30" />
+                            <h2 className={styles.passTag}>Password</h2>
+
+                        </div>
+                        <div className={styles.loginButton}>
+                            <h2 className={styles.loginTag}>Login</h2>
+                        </div>
+
+                        <h4 className={styles.instructorLogin}>Intructor login</h4>
+                    
+                        <h4 className={styles.createAccount}>Create your account</h4>
+                    </div>
+                </div>
 
             </div>
         </>
