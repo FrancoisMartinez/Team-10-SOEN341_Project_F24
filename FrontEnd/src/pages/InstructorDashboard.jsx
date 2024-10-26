d1d032bcf938a47f2279e3bfec20f7293508c055
HaichuanLi <lihaichuan2011@hotmail.com>
2024-10-26 15:36:21 -0400

    removed welcome message

--- FrontEnd/src/pages/InstructorDashboard.jsx
+++ FrontEnd/src/pages/InstructorDashboard.jsx
@@ -242,7 +242,7 @@ function InstructorDashboard() {
 
     return (
         <>
-            {state.user && <p>Welcome {state.user.email}</p>}
+           
 
             <div>
                 <Header></Header>
