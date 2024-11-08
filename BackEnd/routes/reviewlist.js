ee64d520d75c21b683ccb5701520e78af04bbc94
Ayman <mehd.ayman@gmail.com>
2024-11-08 00:01:41 -0500

    Added reviewlist functionality

---
+++ BackEnd/routes/reviewlist.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Review = require('../models/reviewSchema');
+
+
+router.get('/reviews', async (req, res) => {
+    try {
+        const reviews = await Review.find(); // Retrieve all reviews from the database
+        res.status(200).json(reviews); // Send the students as a JSON response
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
